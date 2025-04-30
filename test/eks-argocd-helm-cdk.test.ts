2af10bd3d99e179630e0382081cd5f4ec684531a
hiroshiimaizumi0611 <hiroshiimaizumi0611@gmail.com>
2025-04-30 10:37:47 +0900

    cdk init

---
+++ test/eks-argocd-helm-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EksArgocdHelmCdk from '../lib/eks-argocd-helm-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks-argocd-helm-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EksArgocdHelmCdk.EksArgocdHelmCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
